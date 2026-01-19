83a58ec547e9b13dbfaa90eb7485a8c4af2b1051
Muhammad Uzair Junaid <m.uzairjunaidkhan@technyxsystems.com>
2026-01-19 16:24:50 +0500

    wip

--- src/parser/htmlParser.js
+++ src/parser/htmlParser.js
@@ -2,52 +2,52 @@
 const { parse } = require('node-html-parser');
 
 function parseHTML(diff, filepath) {
-//   const changes = [];
-//   const lines = diff.split('\n');
+  const changes = [];
+  const lines = diff.split('\n');
   
-//   let addedLines = [];
-//   let removedLines = [];
+  let addedLines = [];
+  let removedLines = [];
   
-//   for (const line of lines) {
-//     if (line.startsWith('+') && !line.startsWith('+++')) {
-//       addedLines.push(line.substring(1));
-//     } else if (line.startsWith('-') && !line.startsWith('---')) {
-//       removedLines.push(line.substring(1));
-//     }
-//   }
+  for (const line of lines) {
+    if (line.startsWith('+') && !line.startsWith('+++')) {
+      addedLines.push(line.substring(1));
+    } else if (line.startsWith('-') && !line.startsWith('---')) {
+      removedLines.push(line.substring(1));
+    }
+  }
   
-//   // Detect tag changes
-//   const addedTags = extractTags(addedLines.join('\n'));
-//   const removedTags = extractTags(removedLines.join('\n'));
+  // Detect tag changes
+  const addedTags = extractTags(addedLines.join('\n'));
+  const removedTags = extractTags(removedLines.join('\n'));
   
-//   addedTags.forEach(tag => {
-//     if (!removedTags.includes(tag)) {
-//       changes.push(`Added element: <${tag}>`);
-//     }
-//   });
+  addedTags.forEach(tag => {
+    if (!removedTags.includes(tag)) {
+      changes.push(`Added element: <${tag}>`);
+    }
+  });
   
-//   removedTags.forEach(tag => {
-//     if (!addedTags.includes(tag)) {
-//       changes.push(`Removed element: <${tag}>`);
-//     }
-//   });
+  removedTags.forEach(tag => {
+    if (!addedTags.includes(tag)) {
+      changes.push(`Removed element: <${tag}>`);
+    }
+  });
   
-//   // Detect class/ID changes
-//   const addedClasses = extractClasses(addedLines.join('\n'));
-//   const removedClasses = extractClasses(removedLines.join('\n'));
+  // Detect class/ID changes
+  const addedClasses = extractClasses(addedLines.join('\n'));
+  const removedClasses = extractClasses(removedLines.join('\n'));
   
-//   addedClasses.forEach(cls => {
-//     if (!removedClasses.includes(cls)) {
-//       changes.push(`Added class: .${cls}`);
-//     }
-//   });
+  addedClasses.forEach(cls => {
+    if (!removedClasses.includes(cls)) {
+      changes.push(`Added class: .${cls}`);
+    }
+  });
   
-//   const addedIds = extractIds(addedLines.join('\n'));
-//   addedIds.forEach(id => {
-//     changes.push(`Added ID: #${id}`);
-//   });
+  const addedIds = extractIds(addedLines.join('\n'));
+  addedIds.forEach(id => {
+    changes.push(`Added ID: #${id}`);
+  });
   
-//   return changes.length > 0 ? changes : ['HTML structure modified'];
+  return changes.length > 0 ? changes : ['HTML structure modified'];
 }
 
 function extractTags(html) {
